ecc5afb069645a875f3f4c97789a98d5bf8aa81c
NCenek2 <ceneknicholas@gmail.com>
2026-02-14 09:10:12 -0500

    Update City Card Mapper

--- src/Features/Roles/Mapper/CityCardMapper.tsx
+++ src/Features/Roles/Mapper/CityCardMapper.tsx
@@ -3,7 +3,7 @@ import usePlayer from "../../../Hooks/usePlayer";
 const CityCardMapper = () => {
   const { selectedCard, selectedCity } = usePlayer();
   return (
-    <div className="d-flex justify-content-center align-items-center">
+    <div className="d-flex justify-content-evenly align-items-center w-100">
       <h4>{selectedCard?.title ?? "No Card"}</h4>
       <h4>{selectedCity?.name ?? "No City"}</h4>
     </div>
